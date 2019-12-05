306a2011c6d75055e268abf500236c3efa4a7a0b
topkoka <topkoka4000@gmail.com>
2019-12-06 01:08:47 +0700

    info v0.1a

---
+++ code/02_Js_defaultParameter.js
@@ -0,0 +1,7 @@
+// Default parameter
+
+const adduser=(name,last,city='bkk')=>{
+    return name + last + "  city = "+city
+}
+console.log(adduser("aditep ",'campira',"udonthani"))
+console.log(adduser("aditep ",'campira'))
